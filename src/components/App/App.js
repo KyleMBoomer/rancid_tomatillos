fd77a53c0f4666c34d6e584df17be8c416307dad
Kevin Nelson <km.nelson@outlook.com>
2024-07-07 20:19:17 -0400

    Added initial CSS styling to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,16 @@
-
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
 import './App.css';
 import MovieCards from '../MovieCards/MovieCard';
 import MoviePage from '../MoviePage/MoviePage';
 
 function App() {
-  const [movies, setMovies] = useState([])
-  const [error, setError] = useState('')
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState('');
   const [selectedMovieId, setSelectedMovieId] = useState(null);
 
   useEffect(() => {
-    getMovies()
-  }, [])
+    getMovies();
+  }, []);
 
   const getMovies = () => {
     fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
@@ -26,22 +25,21 @@ function App() {
         console.log(error.message);
         setError('Whoops, could not fetch your movies. Refresh the page.');
       });
-  }
+  };
 
   const handleMovieClick = (id) => {
     setSelectedMovieId(id);
-  }
+  };
 
   const handleBackClick = () => {
     setSelectedMovieId(null);
-  }
+  };
 
   const selectedMovie = movies.find(movie => movie.id === selectedMovieId);
 
-
   return (
     <main className="App">
-      <header>🍅 Rancid Tomatillos 🍅</header>
+      <header className="App-header">🍅 Rancid Tomatillos 🍅</header>
       {error && <p className="error">{error}</p>}
       {selectedMovie ? (
         <MoviePage movie={selectedMovie} onBackClick={handleBackClick} />
@@ -57,3 +55,4 @@ function App() {
 }
 
 export default App;
+
