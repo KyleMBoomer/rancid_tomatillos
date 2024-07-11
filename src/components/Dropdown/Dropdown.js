e1eb9e0ddb8cc65d0ec13b144339723bb7bf482f
“Kyle <kylemboomer@gmail.com>
2024-07-11 12:41:28 -0500

    Refactor Movie Page Buttons

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -3,16 +3,24 @@ import './Dropdown.css'
 
 const Dropdown = ({ selectedGenre, handleGenreChange }) => {
     return (
-        <select className='genre-dropdown' onChange={handleGenreChange} value={selectedGenre}>
-            <option value="">All Genres</option>
-            <option value="Action">Action</option>
-            <option value="Fantasy">Fantasy</option>
-            <option value="Science Fiction">Science Fiction</option>
-            <option value="Drama">Drama</option>
-            <option value="Horror">Horror</option>
-        </select>
-    )
-}
+        <div className="dropdown-container">
+            <label htmlFor="genre-dropdown">What Would You Like to Watch?</label>
+            <select
+                id="genre-dropdown"
+                className="genre-dropdown"
+                onChange={handleGenreChange}
+                value={selectedGenre}
+            >
+                <option value="">All Genres</option>
+                <option value="Action">Action</option>
+                <option value="Fantasy">Fantasy</option>
+                <option value="Science Fiction">Science Fiction</option>
+                <option value="Drama">Drama</option>
+                <option value="Horror">Horror</option>
+            </select>
+        </div>
+    );
+};
 
 Dropdown.propTypes = {
     selectedGenre: PropTypes.string.isRequired,
