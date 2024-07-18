b2ddb7ec6e947892b0d59bbbfb87102c0f41c40a
“Kyle <kylemboomer@gmail.com>
2024-07-18 11:54:41 -0500

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App.js';
 import reportWebVitals from './reportWebVitals';
@@ -7,8 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename="/rancid_tomatillos"></Router>
+    <Router basename="/rancid_tomatillos">
     <App />
+    </Router>
   </React.StrictMode>
 );
 
