d78445ec021cf26c4db6ab3ee261e058ebe03299
“Kyle <kylemboomer@gmail.com>
2024-07-18 11:47:39 -0500

    Fix Path

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -79,7 +79,7 @@ function App() {
         </header>
         {error && <p className="error">{error}</p>}
         <Routes>
-          <Route exact path="/" element={
+          <Route path="/" element={
             <>
               <SearchBar searchQuery={searchQuery} handleSearchChange={handleSearchChange} />
               {!selectedMovie && (
